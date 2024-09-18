7f0b81f56a0c632d113d27c5c511a7e9da1691bf
Christian Jimenez <cajt@outlook.com>
2024-09-18 11:54:34 -0800

    Exercise 5

--- exercises/ex05.js
+++ exercises/ex05.js
@@ -26,3 +26,22 @@ var shirtSleeve = 8.47; // size M (medium)
 Then print N/A to the console because the measurements don't all match up with one particular size.
 
 */
+const shirtWidth = 18; // size L (large)
+const shirtLength = 28; // size L (large)
+const shirtSleeve = 8.13; // size L (large)
+
+if(shirtWidth == 18 && shirtLength == 28 && shirtSleeve == 8.13){
+    console.log("Size S");
+}else if(shirtWidth == 20 && shirtLength == 29 && shirtSleeve == 8.38){
+    console.log("Size M");
+}else if(shirtWidth == 22 && shirtLength == 30 && shirtSleeve == 8.63){
+    console.log("Size L");
+}else if(shirtWidth == 24 && shirtLength == 31 && shirtSleeve == 8.88){
+    console.log("Size XL");
+}else if(shirtWidth == 26 && shirtLength == 33 && shirtSleeve == 9.63){
+    console.log("Size 2XL");
+}else if(shirtWidth == 28 && shirtLength == 34 && shirtSleeve == 10.13){
+    console.log("Size 3XL");
+}else{
+    console.log("N/A");
+}
